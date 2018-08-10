713a3096af70abd7d871702f05a686bfc061dc69
aurel <aurelien.devaux16@gmail.com>
2018-08-10 21:19:16 +0200

    Décomposition des fichiers SCSS pour une meilleure lisibilité

--- src/SAV/ProcessBundle/Controller/ReceiptController.php
+++ src/SAV/ProcessBundle/Controller/ReceiptController.php
@@ -2,16 +2,29 @@
 
 namespace SAV\ProcessBundle\Controller;
 
-
+use SAV\ProcessBundle\Entity\ParcoursProduit;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 class ReceiptController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('SAVProcessBundle:Receipt:receipt-home.html.twig');
+        $receipt = new ParcoursProduit();
+
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $receipt)
+            ->add('numero_serie', TextType::class, array('required' => false))
+            ->add('numero_bar', TextType::class, array('required' => false))
+            ->add('verify', SubmitType::class)
+            ->getForm()
+        ;
+
+        return $this->render('SAVProcessBundle:Receipt:receipt-home.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     public function checkAction($numero_bar)
