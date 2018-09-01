4d90f5e9826f8b21a9be24f9662cbf250ef7dcd7
aurel <aurelien.devaux16@gmail.com>
2018-09-01 20:47:55 +0200

    Avancement partie réparations

--- src/SAV/ProcessBundle/Controller/RepairsController.php
+++ src/SAV/ProcessBundle/Controller/RepairsController.php
@@ -9,8 +9,107 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RepairsController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return new Response("Repair : ça marche !");
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('SAVProcessBundle:ParcoursProduit');
+
+        $repositoryPieces = $em->getRepository('SAVProcessBundle:PieceDetachee');
+        $pieceDetachees = $repositoryPieces->findAll();
+
+        /*
+         * Affichage lors de la première arrivée
+         */
+        $listRepairs = $repository->getAllRepairs('p.statutReparation', 'DESC');
+
+        /*
+         * Récupération des tags de tri de l'URL
+         */
+        if(!empty($request)) {
+            $statut = $request->query->get('statut');
+            $filter = $request->query->get('filter');
+            $order = $request->query->get('order');
+
+            $listStatuts = array(
+                'all',
+                'repair',
+                'refurbished'
+            );
+
+            $listFilter = array(
+                'p.statutReparation',
+                'p.client',
+                'p.referenceProduit'
+            );
+
+            $listOrder = array(
+                'ASC',
+                'DESC'
+            );
+
+            // Tri des données on fonction des tags de l'URL
+            if (in_array($filter, $listFilter) && in_array($order, $listOrder)) {
+                if (in_array($statut, $listStatuts)) {
+                    switch ($statut) {
+                        case 'all':
+                            $listRepairs = $repository
+                                ->getAllRepairs($filter, $order);
+                            break;
+
+                        case 'repair':
+                            $listRepairs = $repository
+                                ->getRepairs($filter, $order);
+                            break;
+
+                        case 'refurbished':
+                            $listRepairs = $repository
+                                ->getRefurbished($filter, $order);
+                            break;
+
+                        default:
+                            $listRepairs = $repository
+                                ->getAllRepairs($filter, $order);
+                            break;
+                    }
+                }
+            }
+        }
+
+        /*
+         * Cas d'une recherche par N° de série ou N° de BAR
+         */
+        if($request->isMethod('POST'))
+        {
+            $searchForm = $_POST['search'];
+
+            if(preg_match('#([a-z]{3})([0-9]{9})#i', $searchForm))
+            {
+                $bar = preg_replace('#([a-z]{3})([0-9]{9})#i', 'BAR$2', $searchForm);
+                $listRepairs = $repository->findBy(array(
+                    'numeroBar' => $bar
+                ));
+            }
+            elseif(preg_match('#[0-9]{9}#', $searchForm))
+            {
+                $listRepairs = $repository->findBy(array(
+                    'numeroSerie' => $searchForm
+                ));
+            }
+        }
+
+        /*
+         * Importation du comptage de lignes
+         */
+        $countAll = $repository->countTotalRepairs();
+        $countRepairs = $repository->countRepairs();
+        $countRefurbished = $repository->countRefurbished();
+
+        return $this->render('SAVProcessBundle:Repairs:repairs-overview.html.twig', array(
+            'listRepairs' => $listRepairs,
+            'pieceDetachees' => $pieceDetachees,
+            'countAll' => $countAll,
+            'countRepairs' => $countRepairs,
+            'countRefurbished' => $countRefurbished
+        ));
     }
 }
--- src/SAV/ProcessBundle/Repository/ParcoursProduitRepository.php
+++ src/SAV/ProcessBundle/Repository/ParcoursProduitRepository.php
@@ -6,6 +6,13 @@ use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 class ParcoursProduitRepository extends EntityRepository
 {
+    /*
+     * 1 : PARTIE RÉCEPTION
+    */
+
+    /*
+     * Récupérer toutes les réceptions non-importées
+     */
     public function getOverview($filter, $order)
     {
         $qb = $this->createQueryBuilder('p')
@@ -21,50 +28,19 @@ class ParcoursProduitRepository extends EntityRepository
             ->getResult();
     }
 
+    public function countLinesTotal()
+    {
+        return (int) $this->countLinesValid() + $this->countLinesFaibleValeur() + $this->countLinesRejected() + $this->countLinesCommented();
+    }
+
+    /*
+     * Récupérer toutes les réceptions valides non-importées
+     */
     public function getOverviewValid($filter, $order)
     {
         $qb = $this->createQueryBuilder('p')
             ->where('p.statutReception = 1')
             ->andWhere('p.faibleValeur = 0')
-            ->andWhere('p.importe = 0')
-            ->orderBy($filter, $order);
-
-        return $qb
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function getOverviewFaibleValeur($filter, $order)
-    {
-        $qb = $this->createQueryBuilder('p')
-            ->where('p.statutReception = 1')
-            ->andWhere('p.faibleValeur = 1')
-            ->andWhere('p.importe = 0')
-            ->orderBy($filter, $order);
-
-        return $qb
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function getOverviewCommented($filter, $order)
-    {
-        $qb = $this->createQueryBuilder('p')
-            ->where('p.statutReception = 3')
-            ->orWhere('p.statutReception = 4')
-            ->andWhere('p.importe = 0')
-            ->orderBy($filter, $order);
-
-        return $qb
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function getOverviewRejected($filter, $order)
-    {
-        $qb = $this->createQueryBuilder('p')
-            ->where('p.statutReception = 2')
-            ->orWhere('p.statutReception = 4')
             ->andWhere('p.importe = 0')
             ->orderBy($filter, $order);
 
@@ -85,16 +61,20 @@ class ParcoursProduitRepository extends EntityRepository
         return (int) $qb->getQuery()->getSingleScalarResult();
     }
 
-    public function countLinesCommented()
+    /*
+     * Récupérer toutes les réceptions à faible valeur non-importées
+     */
+    public function getOverviewFaibleValeur($filter, $order)
     {
-        $qb = $this->createQueryBuilder('p');
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.statutReception = 1')
+            ->andWhere('p.faibleValeur = 1')
+            ->andWhere('p.importe = 0')
+            ->orderBy($filter, $order);
 
-        $qb->select($qb->expr()->count('p'))
-            ->where('p.statutReception  = 3')
-            ->orWhere('p.statutReception = 4')
-            ->andWhere('p.importe = 0');
-
-        return (int) $qb->getQuery()->getSingleScalarResult();
+        return $qb
+            ->getQuery()
+            ->getResult();
     }
 
     public function countLinesFaibleValeur()
@@ -109,6 +89,50 @@ class ParcoursProduitRepository extends EntityRepository
         return (int) $qb->getQuery()->getSingleScalarResult();
     }
 
+    /*
+     * Récupérer toutes les réceptions avec commentaire non-importées
+     */
+    public function getOverviewCommented($filter, $order)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.statutReception = 3')
+            ->orWhere('p.statutReception = 4')
+            ->andWhere('p.importe = 0')
+            ->orderBy($filter, $order);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function countLinesCommented()
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        $qb->select($qb->expr()->count('p'))
+            ->where('p.statutReception  = 3')
+            ->orWhere('p.statutReception = 4')
+            ->andWhere('p.importe = 0');
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
+
+    /*
+     * Récupérer toutes les réceptions refusées non-importées
+     */
+    public function getOverviewRejected($filter, $order)
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.statutReception = 2')
+            ->orWhere('p.statutReception = 4')
+            ->andWhere('p.importe = 0')
+            ->orderBy($filter, $order);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
     public function countLinesRejected()
     {
         $qb = $this->createQueryBuilder('p');
@@ -121,11 +145,9 @@ class ParcoursProduitRepository extends EntityRepository
         return (int) $qb->getQuery()->getSingleScalarResult();
     }
 
-    public function countLinesTotal()
-    {
-        return (int) $this->countLinesValid() + $this->countLinesFaibleValeur() + $this->countLinesRejected() + $this->countLinesCommented();
-    }
-
+    /*
+     * Récupérer la liste des produits pas encore importés
+     */
     public function getImportList()
     {
         $qb = $this->createQueryBuilder('p')
@@ -138,5 +160,96 @@ class ParcoursProduitRepository extends EntityRepository
         return $qb
             ->getQuery()
             ->getResult();
+    }
+
+    /*
+     * 2 : PARTIE RÉPARATIONS
+     */
+
+    /*
+     * Récupérer toutes les réparations du SAV
+     */
+    public function getAllRepairs($filter, $order)
+    {
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->where('p.statutReparation = 1')
+            ->orWhere('p.statutReparation = 2')
+            ->orWhere('p.statutReparation = 3')
+            ->orWhere('p.statutReparation = 4')
+            ->andWhere($qb->expr()->isNull('p.dateReparation'))
+            ->orderBy($filter, $order);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function countTotalRepairs()
+    {
+        return (int) $this->countRepairs() + $this->countRefurbished();
+    }
+
+    /*
+     * Récupérer tous les produits en RÉPARATION
+     */
+    public function getRepairs($filter, $order){
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->where('p.statutReparation = 1')
+            ->orWhere('p.statutReparation = 3')
+            ->andWhere($qb->expr()->isNull('p.dateReparation'))
+            ->orderBy($filter, $order);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function countRepairs()
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        $qb->select($qb->expr()->count('p'))
+            ->where('p.statutReparation  = 1')
+            ->orWhere('p.statutReparation = 3')
+            ->andWhere($qb->expr()->isNull('p.dateReparation'));
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
+
+    /*
+     * Récupérer tous les produits en RECONDITIONNEMENT
+     */
+    public function getRefurbished($filter, $order){
+        $qb = $this->createQueryBuilder('p');
+        $qb
+            ->where('p.statutReparation = 2')
+            ->orWhere('p.statutReparation = 4')
+            ->andWhere($qb->expr()->isNull('p.dateReparation'))
+            ->orderBy($filter, $order);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function countRefurbished()
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        $qb->select($qb->expr()->count('p'))
+            ->where('p.statutReparation  = 2')
+            ->orWhere('p.statutReparation = 4')
+            ->andWhere($qb->expr()->isNull('p.dateReparation'));
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
+
+    public function searchBar($numeroBar)
+    {
+        $this->findBy(array(
+            'numeroBar' => $numeroBar
+        ));
     }
 }
